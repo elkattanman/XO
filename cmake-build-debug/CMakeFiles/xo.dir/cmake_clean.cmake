445ff49d49026bb97399e5605c7fd29831dd9bef
Mustafa Khaled <asdxasd012@gmail.com>
2019-12-24 17:09:09 -0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/xo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/xo.dir/main.cpp.obj"
+  "xo.pdb"
+  "xo.exe"
+  "xo.exe.manifest"
+  "libxo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/xo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
